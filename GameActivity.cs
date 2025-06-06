9963c26e7fb38b43bc93a475d9e385c23325fc0f
JakubCzes <jakczesnowski@gmail.com>
2025-06-07 01:59:21 +0200

    generator losowych plansz

--- GameActivity.cs
+++ GameActivity.cs
@@ -11,6 +11,7 @@ namespace SudokuMobile
 	[Activity(Label = "GameActivity", ScreenOrientation = ScreenOrientation.Portrait)]
 	public class GameActivity : Activity
     {
+		int[,] solvedBoard = new int[9, 9];
 		protected override void OnCreate(Bundle? savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -29,5 +30,198 @@ namespace SudokuMobile
 
 			SetContentView(Resource.Layout.game_activity);
 		}
-    }
+		private Drawable CreateCellBackground(int row, int col, Color backgroundColor)
+		{
+			int strokeThin = 2;
+			int strokeThick = 6;
+
+			int left = (col % 3 == 0) ? strokeThick : strokeThin;
+			int top = (row % 3 == 0) ? strokeThick : strokeThin;
+			int right = (col == 8) ? strokeThick : strokeThin;
+			int bottom = (row == 8) ? strokeThick : strokeThin;
+
+			var shape = new GradientDrawable();
+			shape.SetColor(backgroundColor);
+
+			// LayerDrawable + insets = sztuczna grubość krawędzi
+			var layer = new LayerDrawable(new Drawable[] { shape });
+			layer.SetLayerInset(0, left, top, right, bottom);
+
+			return layer;
+		}
+		private void GenerateSudoku(int hide, GridLayout grid)
+		{
+			int[,] board = new int[9, 9];
+			Random rand = new Random();
+
+			// Generowanie pełnej planszy
+			if (GenerateCompleteSudoku(board, rand))
+			{
+				Array.Copy(board, solvedBoard, board.Length);
+				// Ukrywanie cyfr (możesz zmienić liczbę)
+				HideDigits(board, hide, rand);
+				FillGridFromBoard(board, hide, grid);
+			}
+		}
+		private bool GenerateCompleteSudoku(int[,] board, Random rand)
+		{
+			return SolveSudoku(board, rand);  // Używamy rozwiązywania Sudoku, by wygenerować pełne rozwiązanie
+		}
+
+		// Funkcja rozwiązująca Sudoku za pomocą rekurencji
+		private bool SolveSudoku(int[,] board, Random rand)
+		{
+			for (int row = 0; row < 9; row++)
+			{
+				for (int col = 0; col < 9; col++)
+				{
+					if (board[row, col] == 0) // Szukamy pustej komórki
+					{
+						List<int> possibleNumbers = Enumerable.Range(1, 9).ToList();
+						ShuffleList(possibleNumbers, rand);  // Tasujemy liczby, aby były losowe
+
+						foreach (int num in possibleNumbers)
+						{
+							if (IsValid(board, row, col, num))  // Jeśli liczba jest poprawna
+							{
+								board[row, col] = num;
+								if (SolveSudoku(board, rand))  // Rekurencyjnie rozwiązujemy
+								{
+									return true;
+								}
+								board[row, col] = 0; // Jeśli nie udało się rozwiązać, cofnij
+							}
+						}
+						return false; // Jeśli nie ma możliwego rozwiązania
+					}
+				}
+			}
+			return true; // Zakończone, jeśli wszystkie komórki zostały wypełnione
+		}
+
+		// Funkcja sprawdzająca, czy liczba jest dozwolona w danej komórce
+		private bool IsValid(int[,] board, int row, int col, int num)
+		{
+			// Sprawdzamy, czy liczba nie występuje w tym wierszu
+			for (int i = 0; i < 9; i++)
+			{
+				if (board[row, i] == num || board[i, col] == num)
+					return false;
+			}
+
+			// Sprawdzamy, czy liczba nie występuje w tej siatce 3x3
+			int startRow = row - row % 3;
+			int startCol = col - col % 3;
+			for (int i = startRow; i < startRow + 3; i++)
+			{
+				for (int j = startCol; j < startCol + 3; j++)
+				{
+					if (board[i, j] == num)
+						return false;
+				}
+			}
+
+			return true;
+		}
+
+		// Funkcja do losowego tasowania listy
+		private void ShuffleList(List<int> list, Random rand)
+		{
+			int n = list.Count;
+			while (n > 1)
+			{
+				n--;
+				int k = rand.Next(n + 1);
+				int value = list[k];
+				list[k] = list[n];
+				list[n] = value;
+			}
+		}
+
+		// Funkcja do ukrywania 40 cyfr na planszy
+		private void HideDigits(int[,] board, int digitsToHide, Random rand)
+		{
+			List<int> positions = new List<int>();
+
+			// Losowe usuwanie cyfr z planszy
+			while (positions.Count < digitsToHide)
+			{
+				int row = rand.Next(9);
+				int col = rand.Next(9);
+				int pos = row * 9 + col;
+
+				if (!positions.Contains(pos) && board[row, col] != 0)
+				{
+					positions.Add(pos);
+					board[row, col] = 0;  // Usuwamy cyfrę
+				}
+			}
+		}
+
+		private void FillGridFromBoard(int[,] board, int count, GridLayout grid)
+		{
+			int size = 112; // wielkość komórki w px (dostosuj do ekranu)
+			int previousRow = 0;
+			int previousCol = 0;
+			int childIndex = 0;
+			EditText selectedCell = null;
+			for (int row = 0; row < 9; row++)
+			{
+				for (int col = 0; col < 9; col++)
+				{
+					EditText cell = new EditText(this);
+
+					cell.SetWidth(size);
+					cell.SetHeight(size);
+					cell.Gravity = Android.Views.GravityFlags.Center;
+					cell.SetTextSize(Android.Util.ComplexUnitType.Sp, 20);
+					cell.Focusable = false;
+					cell.FocusableInTouchMode = false;
+					cell.Clickable = true;
+					cell.LongClickable = false;
+					cell.InputType = Android.Text.InputTypes.Null;
+
+					// Stylizacja obramowań
+					Color baseColor = (board[row, col] != 0) ? Color.White : Color.LightGray;
+					cell.Background = CreateCellBackground(row, col, baseColor);
+					cell.Tag = baseColor.ToArgb();
+
+					GridLayout.LayoutParams p = new GridLayout.LayoutParams();
+					p.RowSpec = GridLayout.InvokeSpec(row);
+					p.ColumnSpec = GridLayout.InvokeSpec(col);
+					cell.LayoutParameters = p;
+
+					int currentRow = row;
+					int currentCol = col;
+
+					cell.Click += (s, e) =>
+					{
+						if (selectedCell != null && selectedCell.Tag is Java.Lang.Integer javaColor)
+						{
+							var color = new Color(javaColor.IntValue());
+							selectedCell.Background = CreateCellBackground(previousRow, previousCol, color);
+						}
+						previousRow = currentRow;
+						previousCol = currentCol;
+						selectedCell = (EditText)s;
+						selectedCell.Background = CreateCellBackground(currentRow, currentCol, Color.LightBlue);
+					};
+
+					grid.AddView(cell);
+					if (board[row, col] != 0)
+					{
+						cell.Text = board[row, col].ToString();
+						cell.Clickable = false;
+					}
+					else
+					{
+						cell.Text = "";
+						cell.Background = CreateCellBackground(row, col, Color.LightGray);
+					}
+
+					childIndex++;
+				}
+			}
+		}
+	}
 }
